fa51b31aff83cbb678476e57e412b69f4f3c5a70
Kiran Mayee <sree.kironmai@gmail.com>
2024-04-25 23:23:21 -0500

    modified

--- demo/src/main/java/com/example/repo/TestRepo.java
+++ demo/src/main/java/com/example/repo/TestRepo.java
@@ -16,14 +16,14 @@ public class TestRepo {
 		return "\t welcome inside repo method of TestRepo Class!!!!";
 	}
 
-//	public List<Employee> employeeList(){
-//		Employee e1= new Employee(1,"ccc");
-//		Employee e2= new Employee(4,"nnn");
-//		Employee e3= new Employee(7,"ppp");
-//		Employee e4= new Employee(1,"ccc");
-//		Employee e5= new Employee(5,"yyy");
-//		
-//		List empList= Arrays.asList(e1,e2,e3,e4,e5);
-//		return empList;
-//	}
+	public List<Employee> employeeList(){
+		Employee e1= new Employee(1,"ccc");
+		Employee e2= new Employee(4,"nnn");
+		Employee e3= new Employee(7,"ppp");
+		Employee e4= new Employee(1,"ccc");
+		Employee e5= new Employee(5,"yyy");
+		
+		List empList= Arrays.asList(e1,e2,e3,e4,e5);
+		return empList;
+	}
 }
